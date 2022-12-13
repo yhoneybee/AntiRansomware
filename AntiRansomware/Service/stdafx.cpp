38cab570946dc54fe55acedf2812fdd4daa32223
yhoney <yhoney@estsecurity.com>
2022-12-13 11:13:32 +0900

    미리 컴파일 된 헤더에 내용 추가

--- AntiRansomware/Service/stdafx.cpp
+++ AntiRansomware/Service/stdafx.cpp
@@ -1,1 +1,62 @@
 #include "stdafx.h"
+
+DWORD GetErrorMessage(DWORD error)
+{
+	return GetErrorMessage(error, _T("(%d) %s"));
+}
+
+DWORD GetErrorMessage(DWORD error, LPCTSTR format)
+{
+	DWORD error_id = ERROR_SUCCESS;
+
+	LPVOID buffer = nullptr;
+
+	error_id = FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		nullptr,
+		error,
+		MAKELANGID(
+			LANG_NEUTRAL,
+			SUBLANG_DEFAULT
+		),
+		(LPTSTR)&buffer,
+		0,
+		nullptr);
+
+	if (error_id == FALSE)
+	{
+		return GetLastError();
+	}
+
+	size_t format_length = 0;
+	size_t buffer_length = 0;
+
+	HRESULT res_format_length = StringCchLength(format, STRSAFE_MAX_CCH, &format_length);
+	if (FAILED(res_format_length))
+	{
+		return res_format_length;
+	}
+
+	HRESULT res_buffer_length = StringCchLength((LPTSTR)buffer, STRSAFE_MAX_CCH, &buffer_length);
+	if (FAILED(res_buffer_length))
+	{
+		return res_buffer_length;
+	}
+
+	LPTSTR message = new TCHAR[format_length + buffer_length + INT_MAXIMUM_POWER + 1 + NULL_SPACE];
+	HRESULT result = StringCchPrintf(message, STRSAFE_MAX_CCH, format, error_id, (LPTSTR)buffer);
+	_tprintf_s(message);
+	SAFE_DELETE_ARRAY(message);
+
+	if (LocalFree(buffer) != nullptr)
+	{
+		return GetLastError();
+	}
+
+	if (FAILED(result))
+	{
+		return result;
+	}
+
+	return ERROR_SUCCESS;
+}
--- AntiRansomware/Service/stdafx.h
+++ AntiRansomware/Service/stdafx.h
@@ -1,15 +1,30 @@
 #pragma once
 
-//	includes
+// macro
+#define SAFE_DELETE(p) { if(p != nullptr) { delete p; p = nullptr; } }
+#define SAFE_DELETE_ARRAY(p) { if(p != nullptr) { delete[] p; p = nullptr; } }
+
+//	include
 //		C++
 #include <iostream>
 
 //		C
 #include <Windows.h>
 #include <tchar.h>
+#include <strsafe.h>
+#include <stdarg.h>
 
 //	alias
 //		typedef
 
 
 //		using
+
+
+//	constant value
+constexpr int NULL_SPACE = 1;
+constexpr int INT_MAXIMUM_POWER = 9;
+
+//	global functions
+DWORD GetErrorMessage(DWORD error);
+DWORD GetErrorMessage(DWORD error, LPCTSTR format);
