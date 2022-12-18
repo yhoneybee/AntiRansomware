4ee3406f444b33fe3bfb38b31c95f000cbf7fa5a
yhoney <yhoney@estsecurity.com>
2022-12-18 23:45:28 +0900

    win x64, x86 별로 진입점 함수 설정

--- AntiRansomware/Service/main.cpp
+++ AntiRansomware/Service/main.cpp
@@ -1,11 +1,15 @@
 #include "stdafx.h"
 #include "TestService.h"
 
-int APIENTRY _tmain(DWORD argc, LPTSTR* argv)
+#if _WIN64
+EXTERN_C int APIENTRY _tmain(DWORD argc, LPTSTR* argv)
+#else
+int main(DWORD argc, LPTSTR* argv)
+#endif
 {
 	setlocale(LC_ALL, "korean");
 
-	_tprintf_s(_T("[ ] _tmain\n"));
+	_tprintf_s(_T("[ ] _tmain(%d)\n"), argc);
 
 	TestService test_service(_T("TestService"), _T("TestServiceDisplay"), _T("Desc"));
 	if (argc == 1)
@@ -14,13 +18,24 @@ int APIENTRY _tmain(DWORD argc, LPTSTR* argv)
 	}
 	else if (argc > 0)
 	{
-		if (lstrcmpi(argv[1], _T("-i")) == false)
+		if (argc == 4)
 		{
-			test_service.Install();
+			if (lstrcmpi(argv[2], _T("-p")) == false)
+			{
+				test_service.ExePath(argv[3]);
+			}
 		}
-		else if (lstrcmpi(argv[1], _T("-u")) == false)
+
+		if (argc == 2 || argc == 4)
 		{
-			test_service.Uninstall();
+			if (lstrcmpi(argv[1], _T("-i")) == false)
+			{
+				test_service.Install();
+			}
+			else if (lstrcmpi(argv[1], _T("-u")) == false)
+			{
+				test_service.Uninstall();
+			}
 		}
 	}
 
