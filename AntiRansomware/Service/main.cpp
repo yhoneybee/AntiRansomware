5b639513cbbff91d70d44972b28cafbe5ae11870
yhoney <yhoney@estsecurity.com>
2022-12-19 10:16:32 +0900

    변경된 내용 오류가 없게 진입점 함수에 적용

--- AntiRansomware/Service/main.cpp
+++ AntiRansomware/Service/main.cpp
@@ -11,31 +11,20 @@ int main(DWORD argc, LPTSTR* argv)
 
 	_tprintf_s(_T("[ ] _tmain(%d)\n"), argc);
 
-	TestService test_service(_T("TestService"), _T("TestServiceDisplay"), _T("Desc"));
+	TestService test_service;
 	if (argc == 1)
 	{
 		BaseService::RunServiceMain(&test_service);
 	}
-	else if (argc > 0)
+	else if (argc == 2)
 	{
-		if (argc == 4)
+		if (lstrcmpi(argv[1], _T("-i")) == false)
 		{
-			if (lstrcmpi(argv[2], _T("-p")) == false)
-			{
-				test_service.ExePath(argv[3]);
-			}
+			test_service.Install(SERVICE_KERNEL_DRIVER, SERVICE_BOOT_START);
 		}
-
-		if (argc == 2 || argc == 4)
+		else if (lstrcmpi(argv[1], _T("-u")) == false)
 		{
-			if (lstrcmpi(argv[1], _T("-i")) == false)
-			{
-				test_service.Install();
-			}
-			else if (lstrcmpi(argv[1], _T("-u")) == false)
-			{
-				test_service.Uninstall();
-			}
+			test_service.Uninstall();
 		}
 	}
 
